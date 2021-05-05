1c3b93070ba8cc194b725f06b460efb10a22f105
PavelAstafyev <72070052+PavelAstafyev@users.noreply.github.com>
2021-05-05 12:26:27 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
+#импортируем всякое из tensorflow и pyplot чисто для визуаизации процесса обучения
 import matplotlib.pyplot as plt
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+#создание модели
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(56, 56, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -18,32 +19,31 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(49, activation='softmax'))
 
+#компиляция модели
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+#создание генераторов для обучения и оценки модели и добавление снижения скорости обучения, если аккуратность перестает расти
 datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = datagen.flow_from_directory('cnn\\train', target_size=(56, 56), batch_size=20, class_mode='categorical')
 val_generator = datagen.flow_from_directory('cnn\\val', target_size=(56, 56), batch_size=20, class_mode='categorical')
 test_generator = datagen.flow_from_directory('cnn\\val', target_size=(56, 56), batch_size=20, class_mode='categorical')
 learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
+
+#собственно обучение
 history = model.fit(train_generator, steps_per_epoch=197, epochs=30, validation_data=val_generator, validation_steps=46, callbacks=[learning_rate_reduction])
 
-print(model.evaluate(val_generator, steps=42)[1])
-
+#визуализация
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
-
 loss = history.history['loss']
 val_loss = history.history['val_loss']
-
 epochs_range = range(30)
-
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
 plt.plot(epochs_range, acc, label='Training Accuracy')
 plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
 plt.title('Training and Validation Accuracy')
-
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
@@ -51,8 +51,10 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+#оценка модели
+print(model.evaluate(val_generator, steps=42)[1])
 
-x = input('save?\n')
-if x:
+#ну и по желанию сохраняем модель
+neploho = int(input('Сохранить модель? 1 - да, 0 - нет  '))
+if neploho:
     model.save('machine.h5')
-
